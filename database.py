e890e0e1fd145040bc22cd08dcb0cb7295b3cf3c
Prashu Chaudhary <chaudharyprashu24@gmail.com>
2025-12-30 10:25:23 +0530

    Fix Railway deployment issues

--- database.py
+++ database.py
@@ -1,8 +1,22 @@
-from sqlalchemy import create_engine
+# from sqlalchemy import create_engine
 
+# from sqlalchemy.orm import sessionmaker
+
+# db_url = "postgresql://postgres:DeadEnd3@localhost:5432/fastapi_productPage"
+# engine = create_engine(db_url)
+
+# session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-db_url = "postgresql://postgres:DeadEnd3@localhost:5432/fastapi_productPage"
-engine = create_engine(db_url)
+# SQLite database for deployment (works everywhere)
+db_url = "sqlite:///./products.db"
+
+engine = create_engine(
+    db_url, 
+    connect_args={"check_same_thread": False}  # Required for SQLite
+)
 
 session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
